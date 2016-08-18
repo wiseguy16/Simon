30ab7cd02671aaf6617e7a2daf9f40b69070fb7b
wiseguy16 <gergusa04@gmail.com>
2016-08-18 14:14:03 -0400

    initial commit

---
+++ Simon/SimonBrain.swift
@@ -0,0 +1,15 @@
+//
+//  SimonBrain.swift
+//  Simon
+//
+//  Created by Gregory Weiss on 8/18/16.
+//  Copyright © 2016 Gregory Weiss. All rights reserved.
+//
+
+import Foundation
+
+class SimonBrain
+{
+    
+    
+}
